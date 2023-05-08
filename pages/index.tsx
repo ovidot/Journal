483d04eb9f3301732a2f128dd01f2d68e2b3b188
Uzezi Henry Ovraiti <uovraiti@yahoo.com>
2023-05-08 13:18:18 +0100

    Created sidebar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
+import { Sidebar } from '@/components/Sidebar'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-    <div className="flex flex-col items-center space-y-4 w-full h-full text-black text-xl">
-      <p> Hello World</p>
-    </div>
+    <div
+      className={""}>
+    <main className="flex min-h-screen max-w-7xl mx-auto ">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Journal Feed */}
+
+      {/* Widget */}
+
+      {/* Modal */}
     </main>
+    </div>
   )
 }
